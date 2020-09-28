4ad5bc0d560ababa3f664c5ff2a94646989ee642
Guest1321 <githubakun686@gmail.com>
2020-09-28 15:45:53 +0700

    add comment "menampilkan halaman awal user"

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	// untuk menampilkan halaman awal user
 	public function index()
 	{
 		$this->load->view('templates/header');
