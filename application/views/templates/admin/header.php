d07228b28f3e5ff0cc0676cbc6f76ff19198774a
Guest1321 <githubakun686@gmail.com>
2020-09-30 10:21:12 +0700

    change title page from mrdn admin -> admin kurikulum

--- application/views/templates/admin/header.php
+++ application/views/templates/admin/header.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>MRDN Admin | <?= $judul; ?></title>
+  <title>Admin Kurikulum | <?= $judul; ?></title>
   <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/node_modules/font-awesome/css/font-awesome.min.css" />
   <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.min.css" />
   <link rel="stylesheet" href="<?= base_url(); ?>assets/admin/node_modules/flag-icon-css/css/flag-icon.min.css" />
