8cf7c57692f73f55cb62fca89f355433e9928a82
BakulSoftware609 <githubakun686@gmail.com>
2020-10-13 18:25:52 +0700

    menambah menu organisasi pada navbar frontend

--- application/views/templates/frontend/topbar.php
+++ application/views/templates/frontend/topbar.php
@@ -66,10 +66,11 @@
                         <ul class="nav navbar-nav navbar-right">
                             <li><a href="<?= base_url()  ?>">Home</a></li>
                             <li class="dropdown hassubmenu">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Shop <span class="fa fa-angle-down"></span></a>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Organisasi <span class="fa fa-angle-down"></span></a>
                                 <ul class="dropdown-menu" role="menu">
-                                    <li><a href="shop.html">Shop Layout</a></li>
-                                    <li><a href="shop-single.html">Shop Single</a></li>
+                                    <li><a href="shop.html">Osis</a></li>
+                                    <li><a href="shop-single.html">Pramuka</a></li>
+                                    <li><a href="shop-single.html">IPNU IPPNU</a></li>
                                 </ul>
                             </li>
                             <li><a href="<?= base_url('auth/login')  ?>">Login</a></li>
