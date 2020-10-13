8c35f548d51d5b986ee6e690184bd558ad1c0902
BakulSoftware609 <githubakun686@gmail.com>
2020-10-13 18:29:19 +0700

    fix semua link ke assets/frontend/login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -14,31 +14,31 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->  
-  <link rel="icon" type="image/png" href="<?= base_url('assets/login/') ?>images/icons/favicon.ico"/>
+  <link rel="icon" type="image/png" href="<?= base_url('assets/frontend/login/') ?>images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>fonts/iconic/css/material-design-iconic-font.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>fonts/iconic/css/material-design-iconic-font.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/animate/animate.css">
 <!--===============================================================================================-->  
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/css-hamburgers/hamburgers.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/animsition/css/animsition.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/animsition/css/animsition.min.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/select2/select2.min.css">
 <!--===============================================================================================-->  
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>vendor/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>vendor/daterangepicker/daterangepicker.css">
 <!--===============================================================================================-->
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>css/util.css">
-  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/login/') ?>css/main.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>css/util.css">
+  <link rel="stylesheet" type="text/css" href="<?= base_url('assets/frontend/login/') ?>css/main.css">
 <!--===============================================================================================-->
 </head>
 <body>
   <div class="limiter">
-    <div class="container-login100" style="background-image: url('<?= base_url('assets/login/') ?>images/bg-01.jpg');">
+    <div class="container-login100" style="background-image: url('<?= base_url('assets/frontend/login/') ?>images/bg-01.jpg');">
       <div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
         <form class="login100-form validate-form" action="<?= base_url('auth/login') ?>" method="post">
           <span class="login100-form-title p-b-49">
@@ -85,21 +85,21 @@
 
   <div id="dropDownSelect1"></div>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/jquery/jquery-3.2.1.min.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/jquery/jquery-3.2.1.min.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/animsition/js/animsition.min.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/animsition/js/animsition.min.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/bootstrap/js/popper.js"></script>
-  <script src="<?= base_url('assets/login/') ?>vendor/bootstrap/js/bootstrap.min.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/bootstrap/js/popper.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/bootstrap/js/bootstrap.min.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/select2/select2.min.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/select2/select2.min.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/daterangepicker/moment.min.js"></script>
-  <script src="<?= base_url('assets/login/') ?>vendor/daterangepicker/daterangepicker.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/daterangepicker/moment.min.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/daterangepicker/daterangepicker.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>vendor/countdowntime/countdowntime.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>vendor/countdowntime/countdowntime.js"></script>
 <!--===============================================================================================-->
-  <script src="<?= base_url('assets/login/') ?>js/main.js"></script>
+  <script src="<?= base_url('assets/frontend/login/') ?>js/main.js"></script>
 
 </body>
 </html>
