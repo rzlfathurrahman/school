331543b52a0914332fb67478a7bf50941ae81d5a
BakulSoftware609 <githubakun686@gmail.com>
2020-10-18 17:30:00 +0700

    menambah plugin datatables

---
+++ application/views/backend/kesiswaan/index.php
@@ -0,0 +1,9 @@
+<!-- partial -->
+<div class="content-wrapper">
+  <h3 class="page-heading mb-4">Hello</h3>
+  <div class="row">
+  	<div class="col-12">
+  		Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aliquid!
+  	</div>
+  </div>
+</div>      	
