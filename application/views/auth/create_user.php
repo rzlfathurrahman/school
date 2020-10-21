e2329ef898a93a66187197d3ba196bba4c187d7c
BakulSoftware609 <githubakun686@gmail.com>
2020-10-21 12:26:18 +0700

    merapihkan syntax view create_user

--- application/views/auth/create_user.php
+++ application/views/auth/create_user.php
@@ -1,57 +1,59 @@
-<h1><?php echo lang('create_user_heading');?></h1>
-<p><?php echo lang('create_user_subheading');?></p>
+<!-- partial -->
+<div class="content-wrapper">
+  <h3 class="page-heading mb-4"><?= lang('create_user_heading');?></h3>
+  <div class="row">
+    <div class="col-12">
+    <p><?= lang('create_user_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+    <?= form_open("auth/create_user");?>
 
-<?php echo form_open("auth/create_user");?>
+          <p>
+                <?= lang('create_user_fname_label', 'first_name');?> <br />
+                <?= form_input($first_name);?>
+                <?= form_error('first_name','<span class="font-weight-bold text-danger">','</span>')  ?>
+          </p>
 
-      <p>
-            <?php echo lang('create_user_fname_label', 'first_name');?> <br />
-            <?php echo form_input($first_name);?>
-      </p>
+          <p>
+                <?= lang('create_user_lname_label', 'last_name');?> <br />
+                <?= form_input($last_name);?>
+                <?= form_error('last_name','<span class="font-weight-bold text-danger">','</span>')  ?>
+          </p>
+          
+          <?php
+          if($identity_column!=='email') {
+              echo '<p>';
+              echo lang('create_user_identity_label', 'identity');
+              echo '<br />';
+              echo form_error('identity','<span class="font-weight-bold text-danger">','</span>');
+              echo form_input($identity); 
+              '</p>';
+          }
+          ?>
 
-      <p>
-            <?php echo lang('create_user_lname_label', 'last_name');?> <br />
-            <?php echo form_input($last_name);?>
-      </p>
-      
-      <?php
-      if($identity_column!=='email') {
-          echo '<p>';
-          echo lang('create_user_identity_label', 'identity');
-          echo '<br />';
-          echo form_error('identity');
-          echo form_input($identity);
-          echo '</p>';
-      }
-      ?>
+          <p>
+                <?= lang('create_user_email_label', 'email');?> <br />
+                <?= form_input($email);?>
+                <?= form_error('email','<span class="font-weight-bold text-danger">','</span>')  ?>
+          </p>
 
-      <p>
-            <?php echo lang('create_user_company_label', 'company');?> <br />
-            <?php echo form_input($company);?>
-      </p>
+          <p>
+                <?= lang('create_user_password_label', 'password');?> <br />
+                <?= form_input($password);?>
+                <?= form_error('password','<span class="font-weight-bold text-danger">','</span>')  ?>
+          </p>
 
-      <p>
-            <?php echo lang('create_user_email_label', 'email');?> <br />
-            <?php echo form_input($email);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_phone_label', 'phone');?> <br />
-            <?php echo form_input($phone);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_label', 'password');?> <br />
-            <?php echo form_input($password);?>
-      </p>
-
-      <p>
-            <?php echo lang('create_user_password_confirm_label', 'password_confirm');?> <br />
-            <?php echo form_input($password_confirm);?>
-      </p>
+          <p>
+                <?= lang('create_user_password_confirm_label', 'password_confirm');?> <br />
+                <?= form_input($password_confirm);?>
+          </p>
 
 
-      <p><?php echo form_submit('submit', lang('create_user_submit_btn'));?></p>
+          <p><?= form_submit('submit', lang('create_user_submit_btn'),['class' => 'btn btn-primary']);?></p>
 
-<?php echo form_close();?>
+    <?= form_close();?>
+    </div>
+  </div>
+</div>        
+
+
+
