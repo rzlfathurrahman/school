da4291f8e7af139faa3b73b62e4497d028f722b7
BakulSoftware609 <githubakun686@gmail.com>
2020-10-21 12:15:29 +0700

    merapikan tampilan create_group

--- application/views/auth/create_group.php
+++ application/views/auth/create_group.php
@@ -1,20 +1,27 @@
-<h1><?php echo lang('create_group_heading');?></h1>
-<p><?php echo lang('create_group_subheading');?></p>
+<!-- partial -->
+<div class="content-wrapper">
+  <h3 class="page-heading mb-4"><?= lang('create_group_heading');?></h3>
+  <div class="row">
+  	<div class="col-12">
+		<p><?= lang('create_group_subheading');?></p>
 
-<div id="infoMessage"><?php echo $message;?></div>
+		<div id="infoMessage"><?= $message;?></div>
 
-<?php echo form_open("auth/create_group");?>
+		<?= form_open("auth/create_group");?>
 
-      <p>
-            <?php echo lang('create_group_name_label', 'group_name');?> <br />
-            <?php echo form_input($group_name);?>
-      </p>
+		      <p>
+		            <?= lang('create_group_name_label', 'group_name');?> <br />
+		            <?= form_input($group_name);?>
+		      </p>
 
-      <p>
-            <?php echo lang('create_group_desc_label', 'description');?> <br />
-            <?php echo form_input($description);?>
-      </p>
+		      <p>
+		            <?= lang('create_group_desc_label', 'description');?> <br />
+		            <?= form_input($description);?>
+		      </p>
 
-      <p><?php echo form_submit('submit', lang('create_group_submit_btn'));?></p>
+		      <p><?= form_submit('submit', lang('create_group_submit_btn'), ['class' => 'btn btn-primary'] );?></p>
 
-<?php echo form_close();?>
+		<?= form_close();?>
+  	</div>
+  </div>
+</div>      	
