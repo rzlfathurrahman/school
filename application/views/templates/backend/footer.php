9ee98aab5e43401d62ddd655a3a689e5bbe2e47c
BakulSoftware609 <githubakun686@gmail.com>
2020-10-21 15:32:27 +0700

    delete google maps js

--- application/views/templates/backend/footer.php
+++ application/views/templates/backend/footer.php
@@ -18,12 +18,17 @@
   <script src="<?= base_url(); ?>assets/backend/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
   <script src="<?= base_url(); ?>assets/backend/node_modules/chart.js/dist/Chart.min.js"></script>
   <script src="<?= base_url(); ?>assets/backend/node_modules/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min.js"></script>
-  <!-- <script src="<?= base_url(); ?>https://maps.googleapis.com/maps/api/js?key=AIzaSyB5NXz9eVnyJOA81wimI8WYE08kW_JMe8g&callback=initMap" async defer></script> -->
   <script src="<?= base_url(); ?>assets/backend/js/off-canvas.js"></script>
   <script src="<?= base_url(); ?>assets/backend/js/hoverable-collapse.js"></script>
   <script src="<?= base_url(); ?>assets/backend/js/misc.js"></script>
   <script src="<?= base_url(); ?>assets/backend/js/chart.js"></script>
   <script src="<?= base_url(); ?>assets/backend/js/maps.js"></script>
+  <script type="text/javascript" src="<?= base_url()  ?>assets/DataTables/datatables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $('#dataTable').DataTable();
+    });
+  </script>
 </body>
 
 </html>
