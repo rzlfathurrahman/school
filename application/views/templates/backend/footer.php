1b0bcf10671e3d681b51cebeb78cc40914ee8858
BakulSoftware609 <githubakun686@gmail.com>
2020-10-23 17:24:11 +0700

    menambah css dan javascript untuk datatable

--- application/views/templates/backend/footer.php
+++ application/views/templates/backend/footer.php
@@ -25,5 +25,27 @@
 <script src="<?= base_url()  ?>assets/backend/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <script src="<?= base_url()  ?>assets/backend/dist/js/demo.js"></script>
+<!-- DataTables -->
+<script src="<?= base_url()  ?>assets/backend/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="<?= base_url()  ?>assets/backend/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="<?= base_url()  ?>assets/backend/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="<?= base_url()  ?>assets/backend/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable({
+      "responsive": true,
+      "autoWidth": false,
+    });
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": true,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+    });
+  });
+</script>
 </body>
 </html>
--- application/views/templates/backend/header.php
+++ application/views/templates/backend/header.php
@@ -13,6 +13,9 @@
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="<?= base_url()  ?>assets/backend/dist/css/adminlte.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="<?= base_url()  ?>assets/backend/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?= base_url()  ?>assets/backend/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <!-- Google Font: Source Sans Pro -->
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
