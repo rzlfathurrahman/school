b1f0e0f61d5a5898092375ef6bdf481554356781
BakulSoftware609 <githubakun686@gmail.com>
2020-10-25 16:58:03 +0700

    menambah javascript untuk toastr dan sweetalert

--- application/views/templates/backend/footer.php
+++ application/views/templates/backend/footer.php
@@ -21,6 +21,10 @@
 <script src="<?= base_url()  ?>assets/backend/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
 <script src="<?= base_url()  ?>assets/backend/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- SweetAlert2 -->
+<script src="<?= base_url()  ?>assets/backend/plugins/sweetalert2/sweetalert2.min.js"></script>
+<!-- Toastr -->
+<script src="<?= base_url()  ?>assets/backend/plugins/toastr/toastr.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url()  ?>assets/backend/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
@@ -47,5 +51,8 @@
     });
   });
 </script>
+<?php if (!empty($notification)): ?>
+  <?= $notification  ?>
+<?php endif; ?>
 </body>
 </html>
