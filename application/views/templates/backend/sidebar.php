c4857f2b4f1f649d1e04ef61f4fecbae15691984
BakulSoftware609 <githubakun686@gmail.com>
2020-10-26 06:44:45 +0700

    menghapus brand logo

--- application/views/templates/backend/sidebar.php
+++ application/views/templates/backend/sidebar.php
@@ -25,14 +25,6 @@
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="<?= base_url()  ?>auth" class="brand-link">
-      <img src="<?= base_url()  ?>assets/backend/dist/img/AdminLTELogo.png"
-           alt="AdminLTE Logo"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
-    </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
