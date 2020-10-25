7c40af943928277e6053e5754855de3da94beb7f
BakulSoftware609 <githubakun686@gmail.com>
2020-10-25 10:39:14 +0700

    menambah controller dashboard untuk halaman awal backend

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public $data = [];
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+	}
+
+	public function index()
+	{
+		echo 'Hello World !';
+	}
+
+}
+
+/* End of file Dashboard.php */
+/* Location: ./application/controllers/Dashboard.php */
