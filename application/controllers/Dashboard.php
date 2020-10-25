4db275e8784c75fe0d753265705a7cc7b9f7680d
BakulSoftware609 <githubakun686@gmail.com>
2020-10-25 17:06:34 +0700

    memindah cek login ke construct dan menambahkan beberapa data untuk keperluan halaman

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -15,11 +15,60 @@ class Dashboard extends CI_Controller {
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
 		$this->lang->load('auth');
+		if (!$this->ion_auth->logged_in())
+		{
+			// redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if (!$this->ion_auth->is_admin()) // remove this elseif if you want to enable this for non-admins
+		{
+			// redirect them to the home page because they must be an administrator to view this
+			show_error('You must be an administrator to view this page.');
+		}
 	}
 
 	public function index()
 	{
-		echo 'Hello World !';
+		// siapkan data user yang aktif
+		$data['user'] = $this->session->userdata();
+		$id_user_aktif = $data['user']['user_id'];
+
+		// ambil data user aktif
+		$data['user'] = $this->ion_auth->user()->result_array();
+
+		// dapatkan grup user saat ini
+		$data['user_groups'] = $this->ion_auth->get_users_groups()->result();
+
+		// info halaman aktif 
+		$data['halaman'] = 'dashboard';
+
+		// judul web
+		$data['judul'] = 'Dashboard ';
+
+		// ambil url aktif
+		$data['url'] = $this->uri->segment_array();
+
+		// atur notif muncul hanya jika dari login
+		if ($this->session->flashdata('message') != null) {
+			$data['notification'] ="<script>
+				$(document).ready(function(){
+					const Toast = Swal.mixin({
+				     toast: true,
+				     position: 'top-end',
+				     showConfirmButton: false,
+				     timer: 3000
+					});
+					 toastr.success('Login Berhasil.')
+				});
+
+			</script>";
+			
+		}
+
+		$this->load->view('templates/backend/header',$data);
+		$this->load->view('templates/backend/sidebar');
+		$this->load->view('backend/index');
+		$this->load->view('templates/backend/footer');
 	}
 
 }
