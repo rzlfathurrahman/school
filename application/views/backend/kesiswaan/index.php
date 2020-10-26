8fc1478791444511d60b48da2497d4bc48273e5b
BakulSoftware609 <githubakun686@gmail.com>
2020-10-26 18:25:33 +0700

    menyesuaikan tampilann profil kesiswaan dengan template admin lte

--- application/views/backend/kesiswaan/index.php
+++ application/views/backend/kesiswaan/index.php
@@ -1,9 +1,30 @@
-<!-- partial -->
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <h3 class="page-heading mb-4">Hello</h3>
-  <div class="row">
-  	<div class="col-12">
-  		Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, aliquid!
-  	</div>
-  </div>
-</div>      	
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Profil Kesiswaan</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <?php foreach ($url as $u): ?>
+              <?php if (count($url) == 1): ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "" : ""  ?>"><a href="#" style="cursor: not-allowed;"><?= $u  ?></a></li>
+              <?php else: ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "active" : ""  ?>"><a href="#" style="cursor: not-allowed;"><?= $u  ?></a></li>
+              <?php endif; ?>
+            <?php endforeach ?>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+
+  </section>
+  <!-- /.content -->
