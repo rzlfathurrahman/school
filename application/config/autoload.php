3137498bde1a9cd7ee94e055cc3bb8a3db858388
BakulSoftware609 <githubakun686@gmail.com>
2020-10-26 18:54:10 +0700

    load kesiswaan_model with alias kesiswaan

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,6 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array();
+$autoload['model'] = array(
+	'kesiswaan_model' => 'kesiswaan'
+);
