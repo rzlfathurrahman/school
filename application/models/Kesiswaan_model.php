7ecfe3307b4db266826e2311158c683c8d8aeef7
BakulSoftware609 <githubakun686@gmail.com>
2020-10-26 18:53:26 +0700

    make model kesiswaan with method getProfilKesiswaan

---
+++ application/models/Kesiswaan_model.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kesiswaan_model extends CI_Model {
+
+	public function getProfilKesiswaan()
+	{
+		return $this->db->get('profil_kesiswaan');
+	}
+
+}
+
+/* End of file Kesiswaan_model.php */
+/* Location: ./application/models/Kesiswaan_model.php */
