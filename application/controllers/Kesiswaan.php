310f1ef57a8914d1dbf9e077f058bf0014a7a4d8
BakulSoftware609 <githubakun686@gmail.com>
2020-10-27 05:58:54 +0700

    menambah data profil kesiswaan dan menambah method setProfilKesiswaan

--- application/controllers/Kesiswaan.php
+++ application/controllers/Kesiswaan.php
@@ -36,12 +36,58 @@ class Kesiswaan extends CI_Controller {
 			// data url
 			$data['url'] = $this->uri->segment_array();
 
+			// ambil data profil kesiswaan
+			$data['profil_kesiswaan'] = $this->kesiswaan->getProfilKesiswaan();
+
 			$this->load->view('templates/backend/header',$data);
 			$this->load->view('templates/backend/sidebar');
 			$this->load->view('backend/kesiswaan/index');
 			$this->load->view('templates/backend/footer');
 		}
 	}
+
+	public function ubahProfilKesiswaan()
+	{
+		// tangkap inputan 
+		$id = $this->input->post('id');
+		$judul = $this->input->post('judul');
+		$keterangan = $this->input->post('keterangan');
+
+		// masukan inputan kedalam var data
+		$data = [
+			'id' => (int)$id,
+			'judul' => $judul,
+			'keterangan' => $keterangan
+
+		];
+
+		// masukan id ke variable where
+		$where = ['id' => $id];
+
+		// if ($this->ion_auth->is_WKS4()) {
+		// 	echo 'aku wks 4';
+		// 	exit();
+		// }
+
+		// cek apakah yang menginput yang punya akses
+		if (!$this->ion_auth->logged_in() || !$this->ion_auth->is_admin())
+		{
+			redirect('auth', 'refresh');
+		}
+
+		// validasi form
+		$this->form_validation->set_rules('judul', 'judul', 'trim|required');
+		$this->form_validation->set_rules('keterangan','keterangan', 'trim|required');
+
+		// jika lolos maka update data
+		if ($this->form_validation->run() === TRUE) {
+			$this->kesiswaan->setProfilKesiswaan($data);
+			$this->session->set_flashdata('message','berhasil iupdate');
+		}
+		redirect('kesiswaan','refresh');
+		
+	}
+	
 
 }
 
