a37fa499778769f0c7e02883828264387a8829f3
BakulSoftware609 <githubakun686@gmail.com>
2020-10-27 06:35:12 +0700

    menambah method updateProfilKesiswaan pada model

--- application/models/Kesiswaan_model.php
+++ application/models/Kesiswaan_model.php
@@ -8,6 +8,13 @@ class Kesiswaan_model extends CI_Model {
 		return $this->db->get('profil_kesiswaan')->result();
 	}
 
+	public function updateProfilKesiswaan($data)
+	{
+		$this->db->set($data);
+		$this->db->update('profil_kesiswaan');
+		return true;
+	}
+
 }
 
 /* End of file Kesiswaan_model.php */
