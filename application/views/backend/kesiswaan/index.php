a8613eb5caca31ff9783802227a78e71be0ef8eb
BakulSoftware609 <githubakun686@gmail.com>
2020-10-27 06:01:29 +0700

    menambah view untuk bakcend profil kesiswaan

--- application/views/backend/kesiswaan/index.php
+++ application/views/backend/kesiswaan/index.php
@@ -24,7 +24,61 @@
 
   <!-- Main content -->
   <section class="content">
+    <div class="row">
+       <div class="col-md-12">
+         <div class="card card-outline card-info">
+           <div class="card-header">
+             <h3 class="card-title">
+               Silahkan isi profil kesiswaan dengan editor dibawah.
+             </h3>
+             <!-- tools box -->
+             <div class="card-tools">
+               <button type="button" class="btn btn-tool btn-sm" data-card-widget="collapse" data-toggle="tooltip"
+                       title="Collapse">
+                 <i class="fas fa-minus"></i></button>
+               <button type="button" class="btn btn-tool btn-sm" data-card-widget="remove" data-toggle="tooltip"
+                       title="Remove">
+                 <i class="fas fa-times"></i></button>
+             </div>
+             <!-- /. tools -->
+           </div>
+           <!-- /.card-header -->
+           <div class="card-body pad">
+             <div class="mb-3">
+              <form action="<?= base_url('kesiswaan/setProfilKesiswaan')  ?>">
+                <?php foreach ($profil_kesiswaan as $profil): ?> 
 
+                  <div class="row">
+                    <div class="col-6">
+                      <div class="form-group">
+                        <label for="judul">Judul</label>
+                        <input type="text" id="judul" class="form-control" name="judul" value="<?= $profil->judul  ?>">
+                      </div>
+                    </div>
+                    <div class="col-6">
+                      <div class="form-group">
+                        <label for="url">Url</label>
+                        <input type="text" id="url" class="form-control" name="url" value="<?= $this->uri->uri_string()  ?>">
+                      </div>
+                    </div>
+                  </div>
+                  
+                  <div class="form-group">                
+                   <textarea class="textarea" placeholder="Place some text here"
+                             style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+                        <?= $profil->keterangan  ?>
+                    </textarea>
+                  </div>
 
+                <?php endforeach ?>
+                <button type="submit">submit</button>
+              </form>
+             </div>
+           </div>
+         </div>
+       </div>
+       <!-- /.col-->
+     </div>
+     <!-- ./row -->
   </section>
   <!-- /.content -->
