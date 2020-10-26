c71adf7ca269bb1e45405bbde5eb7e6b2e974645
BakulSoftware609 <githubakun686@gmail.com>
2020-10-26 08:03:23 +0700

    meneysuaikan tampilan create group mengikuti template admihn lte

--- application/views/auth/create_group.php
+++ application/views/auth/create_group.php
@@ -1,26 +1,44 @@
-<!-- partial -->
+<!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  <h3 class="page-heading mb-4"><?= lang('create_group_heading');?></h3>
-  <div class="row">
-  	<div class="col-12">
-		<p><?= lang('create_group_subheading');?></p>
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1><?= lang('create_group_heading');?></h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <?php foreach ($url as $u): ?>
+              <?php if (count($url) == 1): ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "" : ""  ?>"><a href="#" style="cursor: not-allowed;"><?= $u  ?></a></li>
+              <?php else: ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "active" : ""  ?>"><a href="#" style="cursor: not-allowed;"><?= $u  ?></a></li>
+              <?php endif; ?>
+            <?php endforeach ?>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
 
-		<?= form_open("auth/create_group");?>
+  <!-- Main content -->
+  <section class="content">
+  	<?= form_open("auth/create_group");?>
 
-		      <p>
-		            <?= lang('create_group_name_label', 'group_name');?> <br />
-		            <?= form_input($group_name);?>
-		            <?= form_error('group_name','<span class="font-weight-bold text-danger">','</span>')  ?>
-		      </p>
+	  <div class="form-group">
+	        <?= lang('create_group_name_label', 'group_name');?> <br />
+	        <?= form_input($group_name);?>
+	        <?= form_error('group_name','<span class="font-weight-bold text-danger">','</span>')  ?>
+	 </div>
 
-		      <p>
-		            <?= lang('create_group_desc_label', 'description');?> <br />
-		            <?= form_input($description);?>
-		      </p>
+	  <div class="form-group">
+	        <?= lang('create_group_desc_label', 'description');?> <br />
+	        <?= form_input($description);?>
+	 </div>
 
-		      <p><?= form_submit('submit', lang('create_group_submit_btn'), ['class' => 'btn btn-primary'] );?></p>
+	  <p><?= form_submit('submit', lang('create_group_submit_btn'), ['class' => 'btn btn-primary'] );?></p>
 
-		<?= form_close();?>
-  	</div>
-  </div>
-</div>      	
+	<?= form_close();?>
+  </section>
+  <!-- /Main COntent -->
