1fcc1a1b426f3551509abd0b6ebbaf49fc258f02
BakulSoftware609 <githubakun686@gmail.com>
2020-10-29 18:09:26 +0700

    membuat tabel daftar menu serta membuat tombol tambah menu

--- application/views/backend/menu/index.php
+++ application/views/backend/menu/index.php
@@ -1,11 +1,11 @@
-<!-- Content Wrapper. Contains page content -->
+<!--- Content Wrapper. Contains page content  -->
 <div class="content-wrapper">
   <!-- Content Header (Page header) -->
   <section class="content-header">
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1>Profil Kesiswaan</h1>
+          <h1>Manajemen Menu</h1>
         </div>
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
@@ -30,7 +30,7 @@
          <div class="card card-outline card-info">
            <div class="card-header">
              <h3 class="card-title">
-               Silahkan isi profil kesiswaan dengan editor dibawah.
+               Tabel "menu" untuk frontend.
              </h3>
              <!-- tools box -->
              <div class="card-tools">
@@ -43,39 +43,44 @@
              </div>
              <!-- /. tools -->
            </div>
-           <!-- /.card-header -->
-           <div class="card-body pad">
-             <div class="mb-3">
-              <form action="<?= base_url('kesiswaan/setProfilKesiswaan')  ?>" method="post">
-                <?php foreach ($profil_kesiswaan as $profil): ?> 
+           <div class="card-body ">
 
-                  <div class="row">
-                    <div class="col-6">
-                      <div class="form-group">
-                        <label for="judul">Judul</label>
-                        <input type="hidden" name="id" value="<?= $profil->id  ?>">
-                        <input type="text" id="judul" class="form-control" name="judul" value="<?= $profil->judul  ?>">
-                      </div>
-                    </div>
-                    <div class="col-6">
-                      <div class="form-group">
-                        <label for="url">Url</label>
-                        <input type="text" id="url" class="form-control" name="url" value="<?= $this->uri->uri_string()  ?>" readonly>
-                      </div>
-                    </div>
-                  </div>
+            <table id="example2" class="table table-bordered table-striped">
+              <thead class="bg-success text-light">
+                <tr align="center">
+                    <th width="30px">#</th>
+                    <th>Nama Menu</th>
+                    <th>Url</th>
+                    <th>Icon</th>
+                    <th>Aksi</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php $no = 1; foreach ($menu as $menu):?>
+                <tr align="center">
+                  <td><?= $no++  ?></td>    
+                  <td><?= htmlspecialchars($menu->nama_menu,ENT_QUOTES,'UTF-8');?></td>
+                  <td><?= htmlspecialchars($menu->url,ENT_QUOTES,'UTF-8');?></td>
+                  <td><?= $menu->icon  ?></td>
+                  <td>
+                    <!-- link untuk edit menu -->
+                    <?= anchor("menu/editMenu/".$menu->id, '<span class="badge badge-pill badge-info">Edit</span>') ;?>
 
-                  <div class="form-group">                
-                   <textarea class="textarea" name="keterangan" placeholder="Place some text here"
-                             style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
-                        <?= $profil->keterangan  ?>
-                    </textarea>
-                  </div>
+                    <!-- link untuk menghapus menu -->
+                    <?= anchor("menu/hapusMenu/".$menu->id, '<span class="badge badge-pill badge-danger">Hapus</span>') ;?>
+                      
+                  </td>
+                </tr>
+              <?php endforeach;?>
+              </tbody>
+            </table>
 
-                <?php endforeach; ?>
-                <button type="submit" class="btn btn-primary">Simpan</button>
-              </form>
-             </div>
+           </div>
+
+           <div class="card-footer">
+             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modal-default">
+                <i class="fas fa-plus"></i> Tambah Menu
+              </button>
            </div>
          </div>
        </div>
@@ -84,3 +89,39 @@
      <!-- ./row -->
   </section>
   <!-- /.content -->
+
+   <div class="modal fade" id="modal-default">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h4 class="modal-title">Tambah Menu</h4>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <form action="<?= base_url('menu/tambahMenu')  ?>" method="post" >
+          <div class="modal-body">
+              <div class="form-group">
+                <label for="namaMenu">Nama Menu</label>
+                <input type="text" name="nama_menu" id="namaMenu" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="urlMenu">Url Menu</label>
+                <input type="text" name="url" id="urlMenu" class="form-control" required>
+              </div>
+              <div class="form-group">
+                <label for="iconMenu">Icon Menu</label>
+                <input type="text" name="icon" id="iconMenu" class="form-control" required>
+              </div>
+          </div>
+          <div class="modal-footer justify-content-between">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+          </div>
+        </form>
+      </div>
+      <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+  </div>
+  <!-- /.modal -->
