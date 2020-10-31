258a4d5ce4df0fe517af751018513748afcd004b
BakulSoftware609 <githubakun686@gmail.com>
2020-10-31 07:21:41 +0700

    membuat construct dan method strukturOrganisasi di controller kesiswaan

--- application/controllers/Kesiswaan.php
+++ application/controllers/Kesiswaan.php
@@ -3,6 +3,18 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Kesiswaan extends CI_Controller {
 
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+	}
+
 	public function index()
 	{
 		if (!$this->ion_auth->logged_in())
@@ -92,6 +104,11 @@ class Kesiswaan extends CI_Controller {
 		
 	}
 	
+	public function strukturOrganisasi()
+	{
+			
+	}
+
 
 }
 
