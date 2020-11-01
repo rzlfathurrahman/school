55c5de33cc7a0458c18f95c2be31dd90ef80e511
BakulSoftware609 <githubakun686@gmail.com>
2020-11-01 13:01:16 +0700

    membuat views untuk daftar ekstrakurikuler

---
+++ application/views/backend/ekstrakurikuler/index.php
@@ -0,0 +1,33 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Ekstrakurikuler</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <?php foreach ($url as $u): ?>
+              <?php if (count($url) == 1): ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "" : ""  ?>"><a href="#"><?= $u  ?></a></li>
+              <?php else: ?>
+                <li class="breadcrumb-item text-capitalize <?= ($u == $halaman) ? "active" : ""  ?>"><a href="#"><?= $u  ?></a></li>
+              <?php endif; ?>
+            <?php endforeach ?>
+          </ol>
+        </div>
+      </div>
+      <?= $this->session->flashdata('message');  ?>
+    </div><!-- /.container-fluid -->
+  </section>
+
+  <!-- Main content -->
+  <section class="content">
+
+    <?php var_dump($ekstrakurikuler) ?>
+
+  </section>
+  <!-- /.content -->
+   	
