c51c168d00ab81132e5e448dc774ab484f4accda
BakulSoftware609 <githubakun686@gmail.com>
2020-11-01 13:00:34 +0700

    memeprbaiki penulisan __construct()

--- application/controllers/Ekstrakurikuler.php
+++ application/controllers/Ekstrakurikuler.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Ekstrakurikuler extends CI_Controller {
 
-	public function__construct()
+	public function __construct()
 	{
 		parent::__construct();
 		$this->load->database();
@@ -17,7 +17,32 @@ class Ekstrakurikuler extends CI_Controller {
 
 	public function index()
 	{
-		
+			// siapkan data user yang aktif
+		$data['user'] = $this->session->userdata();
+		$id_user_aktif = $data['user']['user_id'];
+
+		// ambil data user aktif
+		$data['user'] = $this->ion_auth->user()->result_array();
+
+		// dapatkan grup user saat ini
+		$data['user_groups'] = $this->ion_auth->get_users_groups()->result();
+
+		// info halaman aktif 
+		$data['halaman'] = 'ekstrakurikiler';
+
+		// judul web
+		$data['judul'] = 'Ekstrakurikuler';
+
+		// ambil url aktif
+		$data['url'] = $this->uri->segment_array();
+
+		// ambil daftar ekstra
+		$data['ekstrakurikuler'] = $this->db->get('ekstrakurikuler')->result();
+
+		$this->load->view('templates/backend/header',$data);
+		$this->load->view('templates/backend/sidebar');
+		$this->load->view('backend/ekstrakurikuler/index');
+		$this->load->view('templates/backend/footer');
 	}
 
 }
