84083cd05e936d15716958ebe55ca715f92ebe91
BakulSoftware609 <githubakun686@gmail.com>
2020-11-07 05:48:08 +0700

    menghilangkan properti data pada controller dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -3,8 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	public $data = [];
-
 	public function __construct()
 	{
 		parent::__construct();
