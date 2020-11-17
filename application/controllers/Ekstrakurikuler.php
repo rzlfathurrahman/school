72063553e0948524b63671d4f3e81d45bdbd0fe6
BakulSoftware609 <githubakun686@gmail.com>
2020-11-18 03:56:27 +0700

    arahkan ke halaman login jika mengakses halaman ekstrakurikuoler tanpa login

--- application/controllers/Ekstrakurikuler.php
+++ application/controllers/Ekstrakurikuler.php
@@ -13,6 +13,9 @@ class Ekstrakurikuler extends CI_Controller {
 		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
 		$this->lang->load('auth');
+		if (!$this->ion_auth->logged_in()) {
+			redirect('auth/login','refresh');
+		}
 	}			
 
 	public function index()
