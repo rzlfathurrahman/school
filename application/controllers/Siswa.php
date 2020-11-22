19eddb049c44e229ae99421c5d4d1837d1abc4a8
BakulSoftware609 <githubakun686@gmail.com>
2020-11-23 06:26:47 +0700

    membuat controller untuk backend siswa

---
+++ application/controllers/Siswa.php
@@ -0,0 +1,58 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Siswa extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();
+		$this->load->library(['ion_auth', 'form_validation']);
+		$this->load->helper(['url', 'language']);
+
+		$this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
+
+		$this->lang->load('auth');
+		if (!$this->ion_auth->logged_in())
+		{
+			// redirect them to the login page
+			redirect('auth/login', 'refresh');
+		}
+		else if (!$this->ion_auth->is_admin()) // remove this elseif if you want to enable this for non-admins
+		{
+			// redirect them to the home page because they must be an administrator to view this
+			show_error('You must be an administrator to view this page.');
+		}
+	}
+
+	public function index()
+	{
+		// siapkan data user yang aktif
+		$data['user'] = $this->session->userdata();
+		$id_user_aktif = $data['user']['user_id'];
+
+		// ambil data user aktif
+		$data['user'] = $this->ion_auth->user()->result_array();
+
+		// dapatkan grup user saat ini
+		$data['user_groups'] = $this->ion_auth->get_users_groups()->result();
+
+		// info halaman aktif 
+		$data['halaman'] = 'backend/siswa';
+
+		// judul web
+		$data['judul'] = 'Siswa';
+
+		// ambil url aktif
+		$data['url'] = $this->uri->segment_array();
+
+		$this->load->view('templates/backend/header',$data);
+		$this->load->view('templates/backend/sidebar');
+		$this->load->view('backend/siswa/index');
+		$this->load->view('templates/backend/footer');
+	}
+
+}
+
+/* End of file Siswa.php */
+/* Location: ./application/controllers/Siswa.php */
