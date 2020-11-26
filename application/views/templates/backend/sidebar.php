5b7b6dd30e27086ae2996cb16f80a690794ed6c7
BakulSoftware609 <githubakun686@gmail.com>
2020-11-27 06:30:20 +0700

    menghapus hamburger menu

--- application/views/templates/backend/sidebar.php
+++ application/views/templates/backend/sidebar.php
@@ -9,17 +9,6 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
     </ul>
-    <!-- Hamburger menu -->
-
-
-    <!-- Right navbar links -->
-<!--     <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-    </ul> -->
   </nav>
   <!-- /.navbar -->
 
