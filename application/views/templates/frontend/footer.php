6666dc611be89a9f0573cd9e220f27d6e76c8a7a
BakulSoftware609 <githubakun686@gmail.com>
2020-11-29 08:15:54 +0700

    membuat javascript untuk preview image

--- application/views/templates/frontend/footer.php
+++ application/views/templates/frontend/footer.php
@@ -15,5 +15,34 @@
 <script src="<?= base_url('assets/backend/')  ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?= base_url('assets/backend/')  ?>dist/js/adminlte.min.js"></script>
+<!-- custom js -->
+<script>
+	function img_preview_ortu () {
+		const input_ortu = document.querySelector('#img-input-ortu');
+		const img_ortu = document.querySelector('#img-preview-ortu');
+		const file_img_ortu =	new FileReader();
+		file_img_ortu.readAsDataURL(input_ortu.files[0]);
+
+		file_img_ortu.onload = function (e) {
+			img_ortu.src = e.target.result;
+		}
+	}
+	function img_preview_siswa(){
+		const input_siswa = document.querySelector('#img-input-siswa');
+
+		const img_siswa = document.querySelector('#img-preview-siswa');
+
+
+		const file_img_siswa =	new FileReader();
+		file_img_siswa.readAsDataURL(input_siswa.files[0]);
+
+		file_img_siswa.onload = function (e) {
+			img_siswa.src = e.target.result;
+		}
+	}
+
+
+
+</script>
 </body>
 </html>
